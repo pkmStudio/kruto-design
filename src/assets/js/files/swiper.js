79045dcf7fc9bcf52a9a5322b8b594d00ddd44a2
Edvard <pmkstudio77@gmail.com>
2025-06-10 15:56:49 +0300

    Все технические страницы готовы

--- src/assets/js/files/swiper.js
+++ src/assets/js/files/swiper.js
@@ -99,6 +99,86 @@ function initSliders() {
             }
         });
     }
+
+    // Проверяем, есть ли слайдер на странице
+    if (document.querySelector('.otzivi-slider') && window.innerWidth < 768) { // Указываем скласс нужного слайдера
+        // Создаем слайдер
+        new Swiper('.otzivi-slider', { // Указываем скласс нужного слайдера
+            // Подключаем модули слайдера
+            // для конкретного случая
+            modules: [Pagination],
+            slidesPerView: 'auto',
+            spaceBetween: 20,
+            autoHeight: true,
+            loop: true,
+            speed: 800,
+
+            //touchRatio: 0,
+            //simulateTouch: false,
+            //loop: true,
+            //preloadImages: false,
+            //lazy: true,
+
+            /*
+            // Эффекты
+            effect: 'fade',
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: false,
+            },
+            */
+
+            // Пагинация
+
+            // pagination: {
+            //     el: '.swiper-pagination',
+            //     clickable: true,
+            // },
+
+
+            // Скроллбар
+            // scrollbar: {
+            //     el: '.swiper-scrollbar',
+            //     draggable: true,
+            // },
+
+
+            // Кнопки "влево/вправо"
+            // navigation: {
+            //     prevEl: '.swiper-button-prev',
+            //     nextEl: '.swiper-button-next',
+            // },
+
+            // Брейкпоинты
+            /*
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                    spaceBetween: 0,
+                    autoHeight: true,
+                },
+                768: {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                },
+                992: {
+                    slidesPerView: 3,
+                    spaceBetween: 20,
+                },
+                1268: {
+                    slidesPerView: 4,
+                    spaceBetween: 30,
+                },
+            },
+            */
+            // События
+            on: {
+
+            }
+        });
+    }
+
+
 }
 // Скролл на базе слайдера (по классу swiper_scroll для оболочки слайдера)
 function initSlidersScroll() {
